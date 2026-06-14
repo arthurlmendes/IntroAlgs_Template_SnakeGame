8c6cbb3d4d43af974f2c14bc088a9091461e3e38
Arthur Lima Mendes <arthurlm115@gmail.com>
2026-06-14 12:04:33 -0300

    Adiciona testes unitarios para a logica do jogo

--- tests/test_logica.py
+++ tests/test_logica.py
@@ -1,31 +1,24 @@
-from src.funcoes import calcular_pontos, jogador_perdeu, limitar_valor
+from src.funcoes import mover_cobra, verificar_colisao_bordas, verificar_colisao_autofagia
 
+def test_movimentacao_cobra_para_direita():
+    corpo_inicial = [[100, 100], [80, 100]]
+    # Move para a direita adicionando 20px no eixo X da cabeça
+    novo_corpo = mover_cobra(corpo_inicial, "DIREITA", 20)
+    assert novo_corpo[0] == [120, 100]
 
-def test_calcular_pontos():
-    """Deve somar corretamente os pontos atuais com os pontos ganhos."""
-    assert calcular_pontos(10, 5) == 15
+def test_colisao_com_borda_esquerda():
+    cabeca_fora = [-20, 100]
+    assert verificar_colisao_bordas(cabeca_fora, 600, 600) is True
 
+def test_colisao_com_borda_direita():
+    cabeca_fora = [600, 100]
+    assert verificar_colisao_bordas(cabeca_fora, 600, 600) is True
 
-def test_jogador_perdeu_com_zero_vidas():
-    """Deve indicar derrota quando o total de vidas chega a zero."""
-    assert jogador_perdeu(0) is True
+def test_autofagia_detectada():
+    # Cabeça [100, 100] ocupando o mesmo lugar de um segmento do corpo
+    corpo_colidindo = [[100, 100], [120, 100], [120, 120], [100, 100]]
+    assert verificar_colisao_autofagia(corpo_colidindo) is True
 
-
-def test_jogador_nao_perdeu_com_vidas():
-    """Nao deve indicar derrota quando o jogador ainda tem vidas."""
-    assert jogador_perdeu(3) is False
-
-
-def test_limitar_valor_abaixo_do_minimo():
-    """Deve retornar o limite minimo quando o valor informado for menor."""
-    assert limitar_valor(-5, 0, 100) == 0
-
-
-def test_limitar_valor_acima_do_maximo():
-    """Deve retornar o limite maximo quando o valor informado for maior."""
-    assert limitar_valor(150, 0, 100) == 100
-
-
-def test_limitar_valor_dentro_do_intervalo():
-    """Deve manter o valor original quando ele ja estiver no intervalo."""
-    assert limitar_valor(50, 0, 100) == 50
+def test_autofagia_ausente():
+    corpo_saudavel = [[140, 100], [120, 100], [100, 100]]
+    assert verificar_colisao_autofagia(corpo_saudavel) is False
